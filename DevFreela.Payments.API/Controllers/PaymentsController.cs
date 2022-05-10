cb4f637724bb1ce9e0ee3d524240c110fbbcaadd
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2022-05-10 16:34:37 -0300

    feat: init project

---
+++ DevFreela.Payments.API/Controllers/PaymentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DevFreela.Payments.API.Controllers
+{
+    [ApiController]
+    [Route("api/payments")]
+    public class PaymentsController : ControllerBase
+    {
+        // [HttpPost]
+        // public async Task<IActionResult> Pay([FromBody] PaymentInfoInputModel paymentInfo)
+        // {
+            
+        // }
+    }
+}
